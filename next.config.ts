93ba4d7d9b12f4cbcbbdd97238633e029bb8ce5e
kirkie05 <kirkie05@gmail.com>
2026-05-06 13:22:04 +0100

    fix: update sentry next.config options for v8

--- next.config.ts
+++ next.config.ts
@@ -78,26 +78,15 @@ export default withSentryConfig(withNextIntl(nextConfig), {
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your quota utilization.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-side errors will fail.
   tunnelRoute: "/monitoring",
+  
+  sourcemaps: {
+    // Hides source maps from visitors
+    deleteMapFilesAfterUpload: true,
+  },
 
-  // Hides source maps from visitors
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with `appDir`)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
 });
 
