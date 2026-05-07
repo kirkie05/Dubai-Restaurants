614551e568a0a1d2e67ce52ebebbc0453623f331
kirkie05 <kirkie05@gmail.com>
2026-05-07 10:23:37 +0100

    fix(sentry): conditionally enable sentry based on presence of SENTRY_AUTH_TOKEN to prevent Vercel build failure

--- next.config.ts
+++ next.config.ts
@@ -62,31 +62,34 @@ const nextConfig: NextConfig = {
 
 import { withSentryConfig } from "@sentry/nextjs";
 
-export default withSentryConfig(withNextIntl(nextConfig), {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+const isSentryEnabled = !!process.env.SENTRY_AUTH_TOKEN;
 
-  org: "dubai-restaurants",
-  project: "dubai-restaurants-app",
+export default isSentryEnabled
+  ? withSentryConfig(withNextIntl(nextConfig), {
+      // For all available options, see:
+      // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
+      org: "dubai-restaurants",
+      project: "dubai-restaurants-app",
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+      // Only print logs for uploading source maps in CI
+      silent: !process.env.CI,
 
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
+      // For all available options, see:
+      // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your quota utilization.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-side errors will fail.
-  tunnelRoute: "/monitoring",
-  
-  sourcemaps: {
-    // Hides source maps from visitors
-    deleteSourcemapsAfterUpload: true,
-  },
+      // Upload a larger set of source maps for prettier stack traces (increases build time)
+      widenClientFileUpload: true,
 
-});
+      // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+      // This can increase your server load as well as your quota utilization.
+      // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-side errors will fail.
+      tunnelRoute: "/monitoring",
+      
+      sourcemaps: {
+        // Hides source maps from visitors
+        deleteSourcemapsAfterUpload: true,
+      },
+    })
+  : withNextIntl(nextConfig);
 
